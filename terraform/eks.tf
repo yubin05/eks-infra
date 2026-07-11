48885fc45171b7569dc207306d55d0269b604166
yubin05 <vundle34@naver.com>
2026-07-12 04:16:32 +0900

    Fix IMDS hop limit for pods and scale node group to 3 for pod density headroom

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -21,12 +21,36 @@ resource "aws_eks_cluster" "main" {
   }
 }
 
+# 기본 hop limit(1)로는 Pod 내부에서 EC2 인스턴스 메타데이터(IMDS)에 접근할 수 없어
+# CloudWatch Agent 등 IMDS로 자격증명을 조회하는 컴포넌트가 실패함 (hop limit 2로 완화)
+resource "aws_launch_template" "node" {
+  name_prefix = "${var.project_name}-node-"
+
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 2
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "${var.project_name}-node"
+    }
+  }
+}
+
 resource "aws_eks_node_group" "main" {
   cluster_name    = aws_eks_cluster.main.name
   node_group_name = "${var.project_name}-node-group"
   node_role_arn   = aws_iam_role.eks_node.arn
   subnet_ids      = aws_subnet.private[*].id
   instance_types  = var.node_instance_types
+
+  launch_template {
+    id      = aws_launch_template.node.id
+    version = aws_launch_template.node.latest_version
+  }
 
   scaling_config {
     desired_size = var.node_desired_size
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -59,7 +59,7 @@ variable "node_instance_types" {
 variable "node_desired_size" {
   description = "Desired number of worker nodes"
   type        = number
-  default     = 2
+  default     = 3
 }
 
 variable "node_min_size" {
